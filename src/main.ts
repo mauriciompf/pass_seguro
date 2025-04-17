35c24c42151af869dd712c71aa7903a9ac621415
unknown <fariasmauricio11@outlook.com>
2025-04-17 15:30:21 -0300

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 const rangeInput = document.querySelector("#range") as HTMLInputElement;
 const rangeValue = document.querySelector(".rangeValue") as HTMLSpanElement;
 
-rangeInput.onchange = async (e) => {
+const updateRangeSliderPosition = (e: Event) => {
   const value = (e.target as HTMLInputElement).value;
 
   const max = 12;
   const min = 4;
-  const totalInterval = max - min;
+
+  const intervalTotal = max - min;
   const relative = Number(value) - min;
+  const percentProgress = (relative * 100) / intervalTotal;
 
-  rangeValue.style = `left: ${(relative * 100) / totalInterval}%`;
+  for (let i = min; i <= max; i++) {
+    if (Number(value) === i) {
+      const alignWithCircle = i - 5 < 0 ? 0 : i - 5;
+
+      rangeValue.style = `left: calc(${percentProgress}% - ${alignWithCircle}%)`;
+    }
+  }
 };
 
-rangeInput.onmousemove = (e) => {
-  //   console.log(e);
-  //   rangeInput.classList.add(`before:left-['${e.clientX}']`);
-};
+rangeInput.onchange = (e) => updateRangeSliderPosition(e);
+rangeInput.onmousemove = (e) => updateRangeSliderPosition(e);
