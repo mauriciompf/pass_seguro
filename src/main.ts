2c0de61c666bd43b5f4c38eed9b4cca0ba7343bd
unknown <fariasmauricio11@outlook.com>
2025-04-21 09:05:55 -0300

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,32 @@
-import updateRangeSliderPosition from "./ts/updateRangeSliderPosition";
+import handleRangeInput from "./ts/handleRangeInput/handleRangeInput";
 
 const rangeInput = document.querySelector("#range") as HTMLInputElement;
-const rangeSlider = document.querySelector(".rangeValue") as HTMLSpanElement;
+const rangeReference = document.querySelector(".rangeValue") as HTMLSpanElement;
 
-rangeInput.onchange = (e) => updateRangeSliderPosition(e, rangeSlider);
-rangeInput.onmousemove = (e) => updateRangeSliderPosition(e, rangeSlider);
+const generatePasswordButton = document.querySelector(
+  ".generatePasswordBtn"
+) as HTMLButtonElement;
+const passwordButton = document.querySelector(
+  ".passwordBtn"
+) as HTMLButtonElement;
+
+const randomNumber = (limit: number) => {
+  const password = [];
+
+  const generateNumber = () => {
+    return Math.floor(Math.random() * 9);
+  };
+
+  for (let i = 0; i <= limit; i++) {
+    password.push(generateNumber());
+  }
+
+  return password.join("");
+};
+
+generatePasswordButton.addEventListener("click", () => {
+  passwordButton.innerHTML = randomNumber(4);
+});
+
+rangeInput.onchange = (e) => handleRangeInput(e, rangeReference);
+rangeInput.onmousemove = (e) => handleRangeInput(e, rangeReference);
